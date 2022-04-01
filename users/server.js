b655c32c4806eff71fa374fc49429c663c8a80e3
Enrique M. Talavera <kiquetal@gmail.com>
2022-04-01 15:18:41 -0400

    Init server handler

---
+++ users/server.js
@@ -0,0 +1,22 @@
+const AWS = require("aws-sdk");
+const cors = require('@middy/http-cors');
+const middy = require("@middy/core");
+const jsonBodyParser = require('@middy/http-json-body-parser');
+const httpError = require('@middy/http-error-handler');
+const lib = require("./lib");
+const validator = require('@middy/validator');
+
+const util = require("./util");
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const httpErrorHandler = require("@middy/http-error-handler");
+dayjs.extend(utc);
+
+const obtainItems = (event,context) => {
+
+    
+
+
+}
+
+exports.obtainItems = middy(obtainItems).use(cors()).use(httpErrorHandler());
